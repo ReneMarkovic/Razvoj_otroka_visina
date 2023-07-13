70ee15f9ff8c947d2dd844529ffda18675767528
Rene Markovič <41106629+ReneMarkovic@users.noreply.github.com>
2023-07-13 21:01:42 +0200

    Spol

--- mf.py
+++ mf.py
@@ -347,8 +347,8 @@ def ui():
     
     spol_options = ("M", "Ž")
     spol_default_index = spol_options.index("M")
-    spol = st.selectbox("Spol otroka", options=spol_options, index=spol_default_index)
-    
+    #spol = st.selectbox("Spol otroka", options=spol_options, index=spol_default_index)
+    spol = st.text_input("Spol otroka (M ali Ž):",value="M")
     st.write("Vnesite starost otroka v letih in mesecih.")
     t_otrok = 12 * st.number_input("Vnesite dopolnjena leta otroka:",value=5)
     t_otrok += st.number_input("Vnesite število mesecev:",value=8)
